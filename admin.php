c5d7ebc70255a961a9155290fe2d1053d305f8a6
Adamsiksik <78469824+Adamsiksik@users.noreply.github.com>
2021-05-03 23:33:34 +0300

    newwww

--- admin.php
+++ admin.php
@@ -14,16 +14,90 @@ $rs=$mysqli->query($sql);
 
 
     <style>
-        table, th, td {
+        body{
+            background-image: url("./img/index.jpg") ;
+            background-repeat: no-repeat;
+            background-size: cover;
+            font-family: fantasy;
+
+        }
+        input[type=text], select {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+            background-color: lavender;
+            font-family: fantasy;
+
+
+        }
+
+        .sup {
+            width: 100%;
+            background-color: darkcyan;
+            color: white;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            font-family: fantasy;
+
+        }
+
+       .sup:hover {
+            background-color: #45a049;
+        }
+
+        div {
+            background-color: transparent;
+        }
+
+        table {
+            margin-top: 10px;
+            margin-bottom: 20px;
+            border-collapse: collapse;
+            width: 100%;
+
+        }
+
+        th, td {
+            text-align: left;
+            padding: 8px;
             border: 1px solid black;
             border-collapse: collapse;
         }
-        th, td {
-            padding: 5px;
-            text-align: left;
-        }
-    </style>
 
+        tr:nth-child(even){background-color: #f2f2f2}
+
+        th {
+            background-color: darkcyan;
+            color: white;
+        }
+        .button {
+            background-color:darkcyan;
+            margin-left: 1600px;
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            cursor: pointer;
+            font-family: fantasy;
+
+        }
+        .te{
+            margin-left: 15px;
+            font-weight: bold;
+            color:darkblue;
+        }
+
+    </style>
 
 
     <!DOCTYPE html>
@@ -33,10 +107,12 @@ $rs=$mysqli->query($sql);
         <title>Title</title>
     </head>
 <body>
+    <a href="p1.html" style="color:black;"> <button class="button" >Logout</button></a>
 <table style="width:100%">
     <tr>
         <th>Name</th>
         <th>Email</th>
+        <th>Password</th>
         <th>PhoneNumber</th>
         <th>Gender</th>
 
@@ -55,6 +131,9 @@ $rs=$mysqli->query($sql);
                 echo " </td>";
                 echo " <td>";
                 printf($row[1]);
+                echo " </td>";
+                echo " <td>";
+                printf($row[2]);
                 echo " </td>";
                 echo " <td>";
                 printf($row[3]);
@@ -88,13 +167,15 @@ $rs=$mysqli->query($sql);
                     $email = $_POST['email'];
                     $PN = $_POST['PhoneNumber'];
                     $pass = $_POST['psw'];
+                    $g = $_POST['gender'];
+
                     echo $name;
                     echo $email;
 
 
                     try {
                         $db = new mysqli('localhost', 'root', '', 'proj');
-                        $qry = "INSERT INTO `member` (`Name`, `PhoneNumber`, `Email`, `Password`, `Gender`) VALUES ('".$name."', '".$PN."', '".$email ."', SHA1('".$pass."'), 'male')";
+                        $qry = "INSERT INTO `member` (`Name`, `PhoneNumber`, `Email`, `Password`, `Gender`) VALUES ('".$name."', '".$PN."', '".$email ."', SHA1('".$pass."'),'".$g ."')";
                         $rs=$db->query($qry);
                         $db->commit();
                         $db->close();
@@ -108,29 +189,39 @@ $rs=$mysqli->query($sql);
         </script>
 
         <form onsubmit=" del()" style="border:1px solid #ccc" method="post" >
-            <label ><b>Email to delete its data</b></label>
+            <label class="te" ><b>Email to delete its data:</b></label>
             <input type="text" onfocus="this.value=''" placeholder="Enter Email" name="email"required>
 
-            <button type="submit" class="submit">delete</button>
+            <button class="sup" type="submit" class="submit">Delete</button>
 
         </form>
 
-        <form onsubmit="add()" style="border:1px solid #ccc" method="post" >
-            <label ><b>Name </b></label>
-            <input type="text" onfocus="this.value=''" placeholder="Enter Name" name="Name"required>
+        <div>
+            <h2 class="te">Add users:</h2>
+            <form onsubmit=" add()" style="border:1px solid #ccc" method="post" >
+                <label class="te" for="Name"> Name</label>
+                <input type="text" id="Name" name="Name" placeholder="Your Name">
 
-            <label ><b>Email </b></label>
-            <input type="text" onfocus="this.value=''" placeholder="Enter Email" name="email"required>
+                <label class="te" for="email">Email</label>
+                <input type="text" id="email" name="email" placeholder="Your email">
 
-            <label ><b>PhoneNumer</b></label>
-            <input type="text" onfocus="this.value=''" placeholder="Enter PN" name="PhoneNumber"required>
+                <label class="te" for="email">Phonenumber</label>
+                <input type="text" id="PhoneNumber" name="PhoneNumber" placeholder="Your phone">
 
-            <label ><b>Password </b></label>
-            <input type="text" onfocus="this.value=''" placeholder="Enter Pass" name="psw"required>
+                <label class="te" for="email">password</label>
+                <input type="text" id="pass" name="psw" placeholder="Your passwrod">
 
-            <button type="submit" class="submit">add</button>
-        </form>
+                <label class="te" for="gender">Gender:</label>
+                <div class="col-75">
+                    <select id="gender" name="gender">
+                        <option value="Male" >male</option>
+                        <option value="Female">Female</option>
+                    </select>
+                </div>
 
+                <input class="sup" type="submit" value="Submit">
+            </form>
+        </div>
         </body>
         </html>
         <?php
